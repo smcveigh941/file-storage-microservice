4ab57baa82ec97a165e042a776a96b3f77f45b58
smcveigh941 <smcveigh941@gmail.com>
2020-12-30 13:59:56 +0000

    add integration tests

---
+++ integration/src/main/java/com/stephenmcveigh/filestorage/integration/helper/FileHelper.java
@@ -0,0 +1,27 @@
+package com.stephenmcveigh.filestorage.integration.helper;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class FileHelper {
+
+  public static File makeFile(int sizeInBytes) throws IOException {
+    File file = new File("testfile");
+    if (file.exists()) {
+      file.delete();
+    }
+    if (file.createNewFile()) {
+      try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw")) {
+        randomAccessFile.write(new byte[sizeInBytes]);
+        return file;
+      }
+    } else {
+      throw new IOException();
+    }
+  }
+
+  private FileHelper() {
+    // Private constructor to hide the implicit public one
+  }
+}
---
+++ integration/src/test/java/com/stephenmcveigh/filestorage/integration/TestFileDownload.java
@@ -0,0 +1,39 @@
+package com.stephenmcveigh.filestorage.integration;
+
+import com.stephenmcveigh.filestorage.integration.helper.FileHelper;
+import com.stephenmcveigh.filestorage.integration.helper.RequestHelper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+class TestFileDownload {
+
+  private static final String POST_FILE_ENDPOINT = "/files";
+  private static final String GET_FILE_ENDPOINT = "/files/%s";
+  private static final int ONE_KB = 1024;
+
+  private File file;
+
+  @AfterEach
+  void tearDown() {
+    if (file != null) {
+      file.delete();
+    }
+  }
+
+  @Test
+  void testFileCanBeDownloaded() throws IOException {
+    file = FileHelper.makeFile(ONE_KB);
+    String fileId = RequestHelper.createFileUploadRequest(file)
+        .post(RequestHelper.getUrl(POST_FILE_ENDPOINT))
+        .getBody().asString();
+
+    RequestHelper.createRequest()
+        .get(RequestHelper.getUrl(String.format(GET_FILE_ENDPOINT, fileId)))
+        .then()
+        .statusCode(200)
+        .contentType("image/png");
+  }
+}
